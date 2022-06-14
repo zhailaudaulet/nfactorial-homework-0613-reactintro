4442887ee7ae8f5736a335bd54c32f9c8c512495
Daulet <daulet.ilchanov@gmail.com>
2022-06-14 12:22:12 +0600

    first commit

---
+++ project-2-app/src/assets/react/index.js
@@ -0,0 +1,9 @@
+const bannerImages={
+    img1: require('./hey.jpeg').default,
+    img2: require('./lets.jpg').default,
+    img3: require('./give.jpeg').default,
+    img4: require('./all.jpeg').default,
+    img5: require('./youCan.jpeg').default,
+
+}
+export default bannerImages;
